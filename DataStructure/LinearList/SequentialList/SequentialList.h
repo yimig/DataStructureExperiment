34acf8b970bd2058b0f92c0ed3e74d9128b6512c
yimig <41770536+yimig@users.noreply.github.com>
2020-07-08 09:55:19 +0800

    修改目录结构

---
+++ DataStructure/LinearList/SequentialList/SequentialList.h
@@ -0,0 +1,54 @@
+//
+// Created by Programmer on 2020/7/8.
+//
+
+#ifndef DATASTRUCTUREEXPERIMENT_SEQUENTIALLIST_H
+#define DATASTRUCTUREEXPERIMENT_SEQUENTIALLIST_H
+#ifndef SequentialList_H
+#define SequentialList_H
+
+#define MAXSIZE 100
+
+
+class SequentialList
+{
+private:
+    char *elem; //指向数据元素的基地址
+    int length; //线性表的当前长度
+
+public:
+
+    //构造一个空顺序表
+    bool InitList();
+
+    //销毁线性表
+    void DestroyList();
+
+    //清空线性表
+    void ClearList();
+
+    //求线性表的长度
+    int GetLength();
+
+    //判断线性表是否为空
+    bool IsEmpty();
+
+    //取值
+    bool GetElem(int i, char &e);//e是取出来的数
+
+    //查找
+    int LocateElem(char e);
+
+    //插入
+    bool Insert(int i, char e);
+
+    //删除
+    bool Delete(int i);
+
+    SequentialList();
+};
+
+#endif
+
+
+#endif //DATASTRUCTUREEXPERIMENT_SEQUENTIALLIST_H
---
+++ DataStructure/LinearList/SequentialList/SequentialListTest.cpp
@@ -0,0 +1,30 @@
+﻿// DataStructure.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "SequentialList.h"
+
+int main()
+{
+    SequentialList list;
+    list.Insert(1, 'H');
+    list.Insert(2, 'e');
+    list.Insert(3, 'l');
+    list.Insert(4, 'l');
+    list.Insert(5, 'o');
+    list.Insert(6, '\0');
+    for(int i=0;i<7;i++)
+    {
+        char e;
+        if(list.GetElem(i,e))
+        {
+            std::cout << e;
+        }
+        else
+        {
+            std::cout << '\0';
+        }
+    }
+    getchar();
+}
+
